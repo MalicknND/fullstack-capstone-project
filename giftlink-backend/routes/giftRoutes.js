b784dcd217da08081dd76f89ca84674e97831891
MalicknND <ndiayemalick062@gmail.com>
2025-01-18 15:12:06 -0500

    save work

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,34 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+
+// Retrieve all gifts
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        // Task 2: Use the collection() method to retrieve the gift collection
+        const collection = db.collection("gifts");
 
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+        // Task 3: Fetch all gifts using the collection.find method
+        const gifts = await collection.find({}).toArray();
 
-        // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        // Task 4: Return the gifts using the res.json method
+        res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
     }
 });
 
+// Retrieve a specific gift by ID
 router.get('/:id', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        // Task 2: Use the collection() method to retrieve the gift collection
+        const collection = db.collection("gifts");
 
         const id = req.params.id;
 
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        // Task 3: Find a specific gift by ID using the collection.findOne method
+        const gift = await collection.findOne({ id: id });
 
         if (!gift) {
             return res.status(404).send('Gift not found');
@@ -41,15 +47,19 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-
-
 // Add a new gift
 router.post('/', async (req, res, next) => {
     try {
+        // Connect to MongoDB
         const db = await connectToDatabase();
+
+        // Retrieve the collection
         const collection = db.collection("gifts");
+
+        // Insert a new gift document
         const gift = await collection.insertOne(req.body);
 
+        // Return the newly added gift
         res.status(201).json(gift.ops[0]);
     } catch (e) {
         next(e);
